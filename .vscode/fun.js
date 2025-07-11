de11162bde3c353af6a5adb277a07a5212f57d1d
Modhi <modhithayeddula@gmail.com>
2025-07-11 10:02:31 +0530

    tA

---
+++ .vscode/fun.js
@@ -0,0 +1,31 @@
+/*function add(a,b){
+    console.log('Addition',a*b);
+    
+}
+add(10,20)
+add(100,"rahul")
+add(50,28000)*/
+function add(a,b){
+    console.log(a+b);
+    
+}
+add(10,20)
+add(100)
+function add(a,b,c=1){
+    console.log(a+b+c);
+}
+add(1,2,3)
+add(1,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ .vscode/fun2.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    let c=100
+    return a+b+c;
+    console.log("good morning");
+    
+}
+let r1=add(10,20)
+console.log(r1);
+let r2=add(1,2)
+console.log(r2);
+
+
+
+
+
+
+
+
+
