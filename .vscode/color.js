3dc8907f13573416d29c5ffdb42a94008adc6301
Modhi <modhithayeddula@gmail.com>
2025-07-16 10:19:22 +0530

    tA

---
+++ .vscode/color.js
@@ -0,0 +1,9 @@
+//let changeColor=()=>{}
+/*function changeColor(){
+    document.getElementById('btn1').innerHTML="Good Morning"
+    document.getElementById('btn1').style.backgroundColor='green'
+    console.log("Have a nice day!")
+}*/
+function changeColor2(){
+    document.getElementsByTagName('button')[1].style.backgroundColor='yellow'
+}
