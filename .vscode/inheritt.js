e327db2b2067873bbb5db1d2824ed0ad15b85469
Modhi <modhithayeddula@gmail.com>
2025-07-23 10:16:27 +0530

    t

--- .vscode/inheritt.js
+++ .vscode/inheritt.js
@@ -21,38 +21,56 @@ class SA extends Account{
     constructor(id,name,address,phno,amount){
         super("Gm","Ga","Ge");
         this.acc_Id=id;
+        this.acc_Name=name;
+        this.acc_Address=address;
         this.acc_mobile=phno;
         this.acc_Bal=amount;
 
     }
     open_Account(){
+        console.log("account is opened");
 
     }
-    deposit_Amount(){
+    deposit_Amount(amount){
+        this.acc_Bal+=amount;
 
     }
     withdrawl(amount){
-
+        this.acc_Bal=this.acc_Bal-amount;
     }
     get_Bal(){
-
+        return this.acc_Bal-this.min_Bal;
     }
 }
 let SA1=new SA(101,'Rahul','Wayand',9988997766,5000)
+SA1.deposit_Amount(100)
+SA1.withdrawl(15)
 console.log(SA1);
-
+console.log(SA1.get_Bal());
 class CA extends Account{
     acc_Id;
     acc_Bal;
     min_Bal=5000;
-    deposit_Amount(){
-
+    constructor(id,name,address,phno,amount){
+        super();
+        this.acc_Id=id;
+        this.acc_Name=name;
+        this.acc_Address=address;
+        this.acc_mobile=phno;
+        this.acc_Bal=amount;
+    }
+    deposit_Amount(amount){
+         this.acc_Bal+=amount;
     }
     withdrawl(amount){
-
+         this.acc_Bal-=amount;
     }
     get_Bal(){
-
+         return this.acc_Bal-this.min_Bal;
     }
 }
-new CA(1001,'Sonia','ND',6675895009,50001)
+let ca=new CA(1001,'Sonia','ND',6675895009,50001)
+ca.deposit_Amount(100)
+ca.withdrawl(15)
+console.log(ca);
+console.log(ca.get_Bal());
